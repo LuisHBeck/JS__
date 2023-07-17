ec887f5b37d2094dc332d77a6ace3d08f8ebaf61
Luís Beck <beck.henrique.luis@gmail.com>
2023-07-17 15:37:01 +0000

    factory func

--- 2 - functions/6 - factory functions/main.js
+++ 2 - functions/6 - factory functions/main.js
@@ -1,12 +1,28 @@
-function createPerson(firtsName, lastName) {
+function createPerson(firtsName, lastName, height, weight) {
     return {
         firtsName,
         lastName,
-        say: function(subject) {
-            return `${firtsName} is ${subject}`
+
+        get completeName() {
+            return `${this.firtsName} ${this.lastName}`;
+        },
+
+        set completeName(value) {
+            value = value.split(" ");
+            this.firtsName = value.shift();
+            this.lastName = value.join(" ")
+        },
+
+        say(subject) {
+            return `${firtsName} is ${subject}`;
+        },
+
+        get imc() {
+            return (weight/(height**2)).toFixed(2);
         }
     };
 }
 
-const p1 = createPerson('Luís', 'Beck');
-console.log(p1)
+const p1 = createPerson('Luís', 'Beck', 1.8, 63);
+(p1.completeName = 'Raphael Kakakak Prtaes')
+console.log(p1.completeName)
